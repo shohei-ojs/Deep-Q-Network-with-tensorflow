de6879de2eb7dd7f555f9644c8645fc086cbf4dc
shohei-ojs <shohei.ojs@gmail.com>
2017-05-23 15:19:53 +0900

    updateqad

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import gym
+
 def main():
+  env = gym.make('Breakout-v0')
+  env.reset()
+  for _ in range(100000000):
+    env.render()
+    env.step(env.action_space.sample())
   pass
 
 
 if __name__ == "__main__":
+
   main()
