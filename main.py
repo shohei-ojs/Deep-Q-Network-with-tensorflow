bf56495872aaeebb86916c7236c8ef02cc8c4c24
shohei-ojs <shohei.ojs@gmail.com>
2017-05-25 15:25:22 +0900

    updated

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import gym
+from models.dqn import DQN
+
 
 def main():
-  env = gym.make('Breakout-v0')
-  env.reset()
-  for _ in range(100000000):
-    env.render()
-    env.step(env.action_space.sample())
+  dqn = DQN()
+  dqn.train()
+
+
+def make_input_data():
   pass
 
 
 if __name__ == "__main__":
-
   main()
